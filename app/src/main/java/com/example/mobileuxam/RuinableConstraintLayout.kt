84676c8b0b0bc48c4b7b77d83fac4dd3db6395f5
HenryClones <henryjones@programmer.net>
2024-12-06 10:57:01 -0500

    Write basic ruiner code

--- app/src/main/java/com/example/mobileuxam/RuinableConstraintLayout.kt
+++ app/src/main/java/com/example/mobileuxam/RuinableConstraintLayout.kt
@@ -1,13 +1,22 @@
 package com.example.mobileuxam
 
 import android.content.Context
+import android.graphics.Canvas
 import android.util.AttributeSet
 import androidx.annotation.AttrRes
 import androidx.constraintlayout.widget.ConstraintLayout
+import kotlin.random.Random
 
 class RuinableConstraintLayout : ConstraintLayout, ViewRuinerComponent {
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
     constructor(context: Context, attrs: AttributeSet?,
                 @AttrRes defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    override fun dispatchDraw(canvas: Canvas) {
+        super.dispatchDraw(canvas)
+        if (Random.nextBoolean()) {
+            canvas.drawColor(0)
+        }
+    }
 }
